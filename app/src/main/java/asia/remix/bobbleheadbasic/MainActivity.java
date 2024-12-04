ba0195af54ca89bd40350567ace5e0f7a3ae42c7
remix <http+github.com@remix.asia>
2024-12-05 05:05:05 +0900

    SpringAnimation.ROTATION_X

--- app/src/main/java/asia/remix/bobbleheadbasic/MainActivity.java
+++ app/src/main/java/asia/remix/bobbleheadbasic/MainActivity.java
@@ -2,15 +2,22 @@ package asia.remix.bobbleheadbasic;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.dynamicanimation.animation.FloatPropertyCompat;//Float No cast "SpringAnimation.ROTATION_X"
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity{
 	final static String TAG= "MainActivity";
+	ImageView imageView;
+	SpringAnimation springAnimation;
 
 	@Override
 	public void onStart(){//onCreate → onStart → onResume ／ onRestart → onStart → onResume
@@ -58,5 +65,32 @@ public class MainActivity extends AppCompatActivity{
 			v.setPadding( systemBars.left, systemBars.top, systemBars.right, systemBars.bottom );
 			return insets;
 		} );
+
+		imageView= findViewById( R.id.imageView );
+		imageView.setOnClickListener( onClickListener );
+		springAnimation= createSpringAnimation( imageView, SpringAnimation.ROTATION_X );
 	}
+
+	SpringAnimation createSpringAnimation( View v, FloatPropertyCompat f ){
+		Log.d( TAG, "createSpringAnimation()" );
+		SpringAnimation sa = new SpringAnimation( v, f );//最終値無し
+		SpringForce sf = new SpringForce();//最終値無し
+		sf.setStiffness( SpringForce.STIFFNESS_MEDIUM );//バネ定数 硬さ
+		sf.setDampingRatio( SpringForce.DAMPING_RATIO_HIGH_BOUNCY / 4 );//減衰比
+		sa.setSpring( sf );
+		return sa;
+	}
+
+	void shakeRestart(){
+		springAnimation.getSpring().setFinalPosition( 180f );
+		springAnimation.setStartValue( -10f );
+		springAnimation.start();
+	}
+
+	View.OnClickListener onClickListener= new View.OnClickListener(){
+		@Override
+		public void onClick( View v ){
+			shakeRestart();
+		}
+	};
 }
